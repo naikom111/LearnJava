58fb14462af83335129e8b7600f8514dc72e1df7
Omprakash <naikom111@gmail.com>
2020-01-29 19:40:16 +0530

    Update to login form

--- GUIS/LoginForm.java
+++ GUIS/LoginForm.java
@@ -6,11 +6,11 @@ public static void main(String[] args)
 JFrame f=new JFrame();
 JLabel j1=new JLabel("Enter Username");
 JLabel j2=new JLabel("Enter Password");
-j1.setBounds(10,100,60,200);
-j2.setBounds(70,100,120,200);
-f.add(j1)
-f.add(j2)
-f.setSize(400,500)
+j1.setBounds(10,100,200,50);
+j2.setBounds(10,300,200,50);
+f.add(j1);
+f.add(j2);
+f.setSize(400,500);
 f.setLayout(null);
 f.setVisible(true);
 }
